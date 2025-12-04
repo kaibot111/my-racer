dc68f7333377068fc2fe8199eecab98b4df0bcf8
Kaibot11 <157419978+kaibot111@users.noreply.github.com>
2025-12-04 12:52:07 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+// Allow connections from anywhere (CORS)
+const io = new Server(server, {
+    cors: {
+        origin: "*", 
+        methods: ["GET", "POST"]
+    }
+});
+
+let players = {};
+
+io.on('connection', (socket) => {
+    console.log('New driver connected:', socket.id);
+
+    // Add new player
+    players[socket.id] = { x: 0, z: 0, angle: 0, color: Math.random() * 0xffffff };
+    
+    // Send current list of players to the new guy
+    socket.emit('currentPlayers', players);
+    
+    // Tell everyone else a new driver joined
+    socket.broadcast.emit('newPlayer', { 
+        id: socket.id, 
+        player: players[socket.id] 
+    });
+
+    // Handle Movement Updates
+    socket.on('playerMovement', (movementData) => {
+        if (players[socket.id]) {
+            players[socket.id].x = movementData.x;
+            players[socket.id].z = movementData.z;
+            players[socket.id].angle = movementData.angle;
+            
+            // Broadcast to everyone else
+            socket.broadcast.emit('playerMoved', {
+                id: socket.id,
+                x: movementData.x,
+                z: movementData.z,
+                angle: movementData.angle
+            });
+        }
+    });
+
+    // Handle Disconnect
+    socket.on('disconnect', () => {
+        console.log('Driver disconnected:', socket.id);
+        delete players[socket.id];
+        io.emit('playerDisconnected', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Race control listening on port ${PORT}`);
+});
